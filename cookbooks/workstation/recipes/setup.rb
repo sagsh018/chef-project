bd20aed09da713f41ea823a1a43628bf16c16fe9
Sagar Sharma <ss0315345@outlook.com>
2020-09-20 20:44:09 +0530

    adding code to setup.rb

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -3,3 +3,18 @@
 # Recipe:: setup
 #
 # Copyright (c) 2020 The Authors, All Rights Reserved.
+
+package "ntp"
+package "tree" do
+    action :install
+end
+
+file "/etc/motd" do
+    content "This server is resource of sagar"
+    owner "root"
+    group "root"
+end
+
+service ntpd do
+    action [:enable, :start]
+end
